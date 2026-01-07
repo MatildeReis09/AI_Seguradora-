269baf2b89c40def4a143c9b234c01a1f4fd8111
Matilde Reis <a28048@alunos.ipca.pt>
2026-01-07 14:49:41 +0000

    again

--- Seguradora.py
+++ Seguradora.py
@@ -1,6 +1,7 @@
 ##Bibliotecas 
 import numpy as np
 import pandas as pd# manipulação de dados
+import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler, OneHotEncoder , LabelEncoder
 ##StandardScaler - nromalizar variaveis numericas
 ## media = 0 e desvio padrao = 1 
@@ -9,9 +10,11 @@ from sklearn.preprocessing import StandardScaler, OneHotEncoder , LabelEncoder
 
 from sklearn.model_selection import train_test_split
 # dividir o data set em teste e treino
+from sklearn.cluster import KMeans # para o algoritmos
 
 df = pd.read_excel("ECF_2.xlsx")
 ## 1- Processo Data cleaning 
+
 
 #definir peso/ importancia das doenças 
 peso_disease = {
@@ -27,15 +30,15 @@ peso_disease = {
     'mental_health': 2 
 }
 
-## indice de doencas
+## criar indice de doencas
 ## juantar todas as doenças e criar um só parametro que as engloba a todas
 df ['index_gravidade'] = 0 
-for disease, peso in peso_disease.item(): ##depara aso pares 
+for disease, peso in peso_disease.item(): 
     df['index_gravidade'] += df[disease] * peso
 ## como no dataset esta (0ou 1) caso tenha ou não a doença apenas se multiplica pelo pelo
 
 ##tratamento de missing values
-df = df.drop(columns=['alcohol_freq'])
+df_original = df.drop(columns=['alcohol_freq'])
 
 # contagem total de nulos na coluna
 nulos_total = df['alcohol_freq'].isnull().sum()
@@ -49,10 +52,10 @@ print(f"\nPercentagem de buracos nos dados: {percentagem:.2f}%")
 ##codificação 
 #aprende as paçavras e associa a numeros
 le = LabelEncoder()
-df['smoker'] = le.fit_transform(df['smoker'])
+df_original['smoker'] = le.fit_transform(df_original['smoker'])
 
 #seleção de parametros para o calculo 
-Calculo_risk_score = [
+Collumns_risk_score = [
     'age',
     'smoker',
     'hospitalizations_last_3yrs',
@@ -60,12 +63,15 @@ Calculo_risk_score = [
     'claims_count', 
 ]
 
-# 4. Preparação do Input do Modelo
-X_features = ['age', 'smoker', 'hospitalizations_last_3yrs', 'index_gravidade', 'claims_count']
-X = df[X_features].copy()
-X = X.fillna(X.mean()) # contra nulos
+
+##criar dataset limpo
+df_trabalho = df_original[Collumns_risk_score ].copy()
 
 ## scaling/normalização 
 scaler = StandardScaler()
-X_scaled = scaler.fit_transform(X)
+X_scaled = scaler.fit_transform(df_trabalho)
+
+
+##algoritmo clustering K-means
+
 
