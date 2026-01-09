d6a9d3baf6313b1e9aa437698a7053eb3c0910e3
Matilde Reis <a28048@alunos.ipca.pt>
2026-01-09 00:09:50 +0000

    1

--- calculo_dadosNULL.py
+++ calculo_dadosNULL.py
@@ -1,6 +1,16 @@
 import numpy as np
 import pandas as pd
 
+
+
+
+try:
+    df = pd.read_excel("excel_dataset/ECF_2.xlsx")
+    print("Ficheiro carregado com sucesso!")
+except Exception as doc : 
+    print(f"Erro ao carregar o ficheiro: {doc}")
+
+
 # contagem total de nulos na coluna
 ## para saber se vale a pena utilizar o parametro 
 
